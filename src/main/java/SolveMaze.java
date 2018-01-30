c34664be90d68923a87715d1458864797276f280
yinchunrui <yrui2@illinois.edu>
2018-01-30 16:23:33 -0600

    solve the maze

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,29 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            if (maze.isFinished()) {
+                break;
+            }
+            maze.turnRight();
+            if (maze.canMove()) {
+                maze.move();
+                continue;
+            } else {
+                maze.turnLeft();
+            }
+            if (maze.canMove()){
+                maze.move();
+                continue;
+            } else {
+                maze.turnLeft();
+            }
+            if (maze.canMove()) {
+                maze.move();
+                continue;
+            } else {
+                maze.turnLeft();
+                maze.move();
+            }
         }
 
         if (maze.isFinished()) {
